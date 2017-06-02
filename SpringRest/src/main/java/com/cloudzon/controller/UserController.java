aa0d713ae70da7564345062626ca8d18fa4b7058
HiteshR <umaletiya.hitesh@gmail.com>
2017-06-02 13:53:29 +0530

    rest controller

--- SpringRest/src/main/java/com/cloudzon/controller/UserController.java
+++ SpringRest/src/main/java/com/cloudzon/controller/UserController.java
@@ -10,13 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cloudzon.dto.AccessTokenContainer;
 import com.cloudzon.dto.ResponseMessageDto;
@@ -26,7 +26,7 @@ import com.cloudzon.exception.AuthorizationException;
 import com.cloudzon.model.User;
 import com.cloudzon.service.UserService;
 
-@Controller
+@RestController	
 @RequestMapping("user")
 public class UserController {
 
